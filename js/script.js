128a3b707addd8df8deda08077918a0fe04ac0bd
AMerkulova <Anastasia@MacBook-Pro-Anastasia-2.local>
2018-04-05 07:29:10 +0400

    JS промежуточный вариант

--- js/script.js
+++ js/script.js
@@ -5,17 +5,11 @@ var form = popup.querySelector("contact-form");
 var name = popup.querySelector("[name=contact-name]");
 var email = popup.querySelector("[name=email]");
 var message = popup.querySelector("[name=message]");
-var isStorageSupport = true;
-var storage = "";
- try {
-    storage = localStorage.getItem("login");
-  } catch (err) {
-    isStorageSupport = false;
-  } 
         
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
+    name.focus();
 });
 
 close.addEventListener("click", function (evt) {
@@ -25,16 +19,25 @@ close.addEventListener("click", function (evt) {
 });
         
 form.addEventListener("submit", function (evt) {
-    if(!name.value) {
-    evt.preventDefault();
-    popup.classList.add("modal-error")
-  }
-  if(!email.value) {
-    evt.preventDefault();
-    popup.classList.add("modal-error")
-  }
-  if(!message.value) {
-    evt.preventDefault();
-    popup.classList.add("modal-error")
+    if (!name.value || !email.value || !message.value) {
+          evt.preventDefault();
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+        } 
+        else {
+          popUp.classList.remove("modal-show");
+          popUp.classList.remove("modal-error");
   }
 });
+
+window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+          }
+        }
+});
+      
